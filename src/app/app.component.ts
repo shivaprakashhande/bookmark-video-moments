a8083a4e8433b5c94ad669edc3c69dcf057365cb
shivaprakashhande <shivaprakashhande@gmail.com>
2018-06-27 12:04:34 +0530

    added googe sign in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ declare var gapi: any;
 export class AppComponent {
   title = 'app';
   profilePic: string;
-  signedIn: boolean = false;
   userName: string;
   public auth2: any;
   public googleInit() {
@@ -33,26 +32,16 @@ export class AppComponent {
 
     this.auth2.attachClickHandler(element, {},
       (googleUser) => {
-        this.signedIn = true;
         let profile = googleUser.getBasicProfile();
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('ID: ' + profile.getId());
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-        //YOUR CODE HERE
         document.getElementById('sign-out').removeAttribute('hidden');
         document.getElementById('profile-pic').style.display = 'initial';
         document.getElementById('profile-pic').setAttribute('src', profile.getImageUrl());
         document.getElementById('user-name').innerHTML = profile.getName();
-
-
       }, (error) => {
         alert(JSON.stringify(error, undefined, 2));
       });
 
     if (this.auth2.isSignedIn.get()) {
-      this.signedIn = true;
       var profile = this.auth2.currentUser.get().getBasicProfile();
       document.getElementById('sign-out').removeAttribute('hidden');
       document.getElementById('profile-pic').style.display = 'initial';
